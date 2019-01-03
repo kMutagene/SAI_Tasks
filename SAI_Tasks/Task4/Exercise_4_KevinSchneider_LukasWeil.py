8484eae751b799e48f4e03952babdb9dff8acbf0
Kevin Schneider <kevin-schneider@mutagene.de>
2019-01-03 17:45:07 +0100

    Add membership functions for T in 4.5

--- SAI_Tasks/Task4/Exercise_4_KevinSchneider_LukasWeil.py
+++ SAI_Tasks/Task4/Exercise_4_KevinSchneider_LukasWeil.py
@@ -2,6 +2,57 @@ import math
 
 from shapely import geometry
 
-poly = geometry.Polygon([(0, 0), (1, 1), [3, 0]])
+def R (c, d, x) : 
+    if (x > d) :
+        return 0.
+    elif (x < c) :
+        return 1.
+    else:
+        return (d - x) / (d - c)
+    
+def L (a, b, x) : 
+    if (x < a):
+        return 0.
+    elif (x > b):
+        return 1.
+    else:
+        return (x - a) /(b - a)
 
-poly.area
+def triang (a, b, m, x) : 
+    if (x <= a):
+        return 0.
+    elif (x >= b):
+        return 0.
+    elif ((a < x) & (x <= m)):
+        return (x - a) / (m - a)
+    else:
+        return (b - x) / (b - m)
+
+def high (temp):
+    return L(30., 40., temp)
+
+def relHigh (temp):
+    return triang(26., 40., 34., tmp)
+
+def med (temp):
+    return triang(16., 34., 25.)
+
+def relLow (temp):
+    return triang(8., 24., 16., temp)
+
+def low (temp):
+    return R(10., 20., temp)
+
+memberships = [
+       high,
+       relHigh,
+       med,
+       relLow,
+       low
+    ]
+
+
+def fuzzy (measured: dict, memberships: list, rules: list, defuzzification: list) :
+    return 0
+
+
